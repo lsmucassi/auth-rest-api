387c1b398a7df305a3910e86363bf44a1a942865
lsmucassi <lindasmucassi@gmail.com>
2020-04-19 13:02:08 +0200

    logins

--- routes/auth.js
+++ routes/auth.js
@@ -34,14 +34,19 @@ router.post('/register', async (req, res) => {
 })
 
 //Login
-router.post('./login', (req, res) => {
-    //Validate before saving User to DB
+router.post('./login', async (req, res) => {
+    //Validate before login
     const {error} = registerValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
     //Check if user already exist
-    const emailExist = await User.findOne({email: req.body.email})
-    if (!emailExist) return res.status(400).send('Error: incorrect email or password!')
+    const user = await User.findOne({email: req.body.email})
+    if (!user) return res.status(400).send('Error: incorrect email or password!')
+    //Check if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send('Error: Password Incorrect')
+
+    res.send('Success login')
 })
 
 module.exports = router
